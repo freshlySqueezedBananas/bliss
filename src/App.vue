c2c2e5ee0418879362ca5ba19d6c7794c9c7e451
Gonçalo Cancela de Abreu <goncalo.abreu@etnos.co>
2017-04-23 20:25:27 +0100

    Fixed 'failed to mount component' error

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="app">
-    <loading-screen v-if='showLoading' :health='health' :dead='dead' :conn='conn' :refreshing='refreshing' @refresh="onRefresh"></loading-screen>
-  <router-view></router-view>
-  </div>
-</template>
-
-<script src="./app.js" lang="babel"></script>
